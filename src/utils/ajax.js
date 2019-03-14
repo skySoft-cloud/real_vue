a4aff48c097814b6d1f259714090ba416a78468a
cloud <772602532@qq.com>
2019-03-14 17:12:06 +0800

    【添加错误处理】

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -7,14 +7,18 @@ let base_url = "../static/test";
  * get请求
  */
 const get = (params)=> {
-  return axios.get(`${base_url}/${params}`).then((res)=>res.data)
+  return axios.get(`${base_url}/${params}`).then((res)=>res.data).catch((err)=> {
+    console.log(err)
+  })
 }
 
 /**
  * post请求
  */
-const post = (params,data)=> {
-  return axios.get(`${base_url}/${params}`,qs.stringify(data)).then((res)=>res.data)
+const post = (params, data)=> {
+  return axios.get(`${base_url}/${params}`, qs.stringify(data)).then((res)=>res.data).catch((err)=> {
+    console.log(err)
+  })
 }
 
 export {
